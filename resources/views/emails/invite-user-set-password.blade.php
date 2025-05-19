7142b239a6d53efb5ef65c526a6e701eb792f3a3
Itamar Junior <hello@itjunior.dev>
2025-05-19 20:21:21 +0100

    Fix indentation in invite user email template

--- resources/views/emails/invite-user-set-password.blade.php
+++ resources/views/emails/invite-user-set-password.blade.php
@@ -1,19 +1,18 @@
 @component('mail::message')
-    # You're Invited to Join {{ $businessName }}
+# You're Invited to Join {{ $businessName }}
 
-    Hello {{ $user->name ?? 'there' }},
+Hello {{ $user->name ?? 'there' }},
 
-    {{ $inviterName }} has invited you to join the **{!! $businessName !!}** team on **{{ config('app.name') }}** as a
-    member.
+{{ $inviterName }} has invited you to join the **{!! $businessName !!}** team on **{{ config('app.name') }}** as a member.
 
-    To get started, please click the button below to set your password and activate your account.
+To get started, please click the button below to set your password and activate your account.
 
 @component('mail::button', ['url' => $url])
     Submit Response
 @endcomponent
 
-    If you weren't expecting this invitation, feel free to ignore this email.
+If you weren't expecting this invitation, feel free to ignore this email.
 
-    Thanks,
-    {{ config('app.name') }} Team
+Thanks,
+{{ config('app.name') }} Team
 @endcomponent
