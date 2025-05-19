359bada7414927d00a2126a96bb7850f496f1dc2
Itamar Junior <hello@itjunior.dev>
2025-05-19 21:36:48 +0100

    Add dropdown menu with delete option to company table rows

--- resources/views/livewire/company/company.blade.php
+++ resources/views/livewire/company/company.blade.php
@@ -117,8 +117,8 @@
                 </div>
             </div>
 
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+            <div class="overflow-x-auto">
+                <table class="w-full h-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
                     wire:loading.class="opacity-50">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -230,7 +230,7 @@
                                         {{ $company->ar_status }}
                                     </span>
                                 </td>
-                                <td class="px-6 py-1.5">
+                                <td class="px-6 py-1.5 flex items-center">
                                     <div wire:click="showCompanySubmissions({{ $company->id }})">
                                         <div
                                             class="relative inline-flex items-center p-3 text-sm font-medium text-center">
@@ -244,6 +244,35 @@
                                             <div
                                                 class="absolute inline-flex items-center justify-center size-6 text-xs font-bold text-white bg-blue-500 border-2 border-white rounded-full -top-0.5 -end-0.5 dark:border-gray-900">
                                                 {{ $company->submissionDocuments->count() }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div x-data="{ dropdown: false }">
+                                        <button id="apple-imac-27-dropdown-button"
+                                            data-dropdown-toggle="apple-imac-27-dropdown"
+                                            @click="dropdown = !dropdown"
+                                            class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                                            type="button">
+                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                            </svg>
+                                        </button>
+                                        <div id="apple-imac-27-dropdown" x-show="dropdown"
+                                            @click.away="dropdown = false"
+                                            class="absolute right-0 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                            {{-- <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                                        aria-labelledby="apple-imac-27-dropdown-button">
+                                        <li>
+                                            <div wire:click="updateSubmission({{ $company->id }})"
+                                                class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                Update Submissions</div>
+                                        </li>
+                                    </ul> --}}
+                                            <div class="py-1">
+                                                <a href="#"
+                                                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white cursor-not-allowed">Delete</a>
                                             </div>
                                         </div>
                                     </div>
