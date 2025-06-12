452d34f7ebcf9a6b4587d90b9cf414958f82f610
Itamar Junior <hello@itjunior.dev>
2025-06-12 10:23:59 +0100

    Add sortable column for company registration number and rename header to CRO

--- resources/views/livewire/company/company.blade.php
+++ resources/views/livewire/company/company.blade.php
@@ -122,8 +122,19 @@
                     wire:loading.class="opacity-50">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-2">
-                                Reg #
+                            <th scope="col" class="px-6 py-2" wire:click="sort('company_number')">
+                                <div class="flex items-center">
+                                    @if ($sortBy === 'company_number')
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor"
+                                            class="size-6 duration-400 transform  ease-in-out
+                                @if ($sortDirection === 'asc' && $sortBy === 'company_number') rotate-180 @endif">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                        </svg>
+                                    @endif
+                                    CRO
+                                </div>
                             </th>
                             <th scope="col" class="px-6 py-2" wire:click="sort('custom')">
                                 <div class="flex items-center">
@@ -221,7 +232,8 @@
                                             {{ $company->name }}
                                         </p>
                                         <span class="text-xm text-gray-500 dark:text-gray-400">
-                                            Address: {{ $company->address_line_1 . ', ' . $company->address_line_2 ?? '' }}
+                                            Address:
+                                            {{ $company->address_line_1 . ', ' . $company->address_line_2 ?? '' }}
                                         </span>
                                         @if ($company->tags->isNotEmpty())
                                             @foreach ($company->tags as $tag)
