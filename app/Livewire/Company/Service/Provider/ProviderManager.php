42bf5ffd6aa31071a24dfaef21793ffe76860ce1
Itamar Junior <hello@itjunior.dev>
2025-09-25 14:54:58 +0100

    Sort service categories by name when loading in ProviderManager

--- app/Livewire/Company/Service/Provider/ProviderManager.php
+++ app/Livewire/Company/Service/Provider/ProviderManager.php
@@ -43,7 +43,8 @@ class ProviderManager extends Component
     public function mount()
     {
         $this->loadProviders();
-        $this->categories = ServiceCategory::where('business_id', Auth::user()->current_business_id)->get();
+        $this->categories = ServiceCategory::where('business_id', Auth::user()->current_business_id)->get()
+            ->sortBy('name');
     }
 
     public function loadProviders()
