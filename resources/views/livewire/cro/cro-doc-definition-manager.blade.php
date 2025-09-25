21640df95fb3ffe3ab2224ecb617758819ccf330
Itamar Junior <hello@itjunior.dev>
2025-09-25 11:50:21 +0100

    Update headings in CRO definition manager and provider heading for consistency

--- resources/views/livewire/cro/cro-doc-definition-manager.blade.php
+++ resources/views/livewire/cro/cro-doc-definition-manager.blade.php
@@ -1,7 +1,7 @@
 <section class="w-full">
     @include('partials.cro-definition-heading')
 
-    <x-cro-definition.layout :heading="__('Companies')" :subheading="__('All companies under your current business')">
+    <x-cro-definition.layout :heading="__('CRO Definitions')" :subheading="__('All definitions related to your CRO')">
         {{-- Flash messages --}}
         @if (session('success'))
             <div class="mb-4">
@@ -20,14 +20,15 @@
         @endif
 
         <div>
-            <div class="flex justify-between items-center mb-4">
-                <input type="text" wire:model.debounce.500ms="search" placeholder="Search definitions..."
+            <div class="flex justify-between mb-4 space-x-4">
+                <input type="text" wire:model.live.debounce.500ms="search" placeholder="Search definitions..."
                     class="border rounded p-2" />
-                <button wire:click="showCreateModal()" class="bg-blue-600 text-white px-4 py-2 rounded">New
-                    Definition</button>
+                <button wire:click="showCreateModal()" class="bg-blue-600 text-white px-4 py-2 rounded">
+                    New Definition
+                </button>
             </div>
 
-            <table class="w-full table-auto">
+            <table class="w-full text-left table-auto min-w-max" wire:loading.class="opacity-50">
                 <thead>
                     <tr class="bg-gray-100">
                         <th class="px-4 py-2">Name</th>
@@ -66,6 +67,9 @@
                 <h3 class="text-lg font-semibold mb-4">
                     {{ $modelId ? 'Edit Definition' : 'New Definition' }}
                 </h3>
+                <span class="text-sm text-gray-600 mb-4 block">
+                    {{ $modelId ? 'Update the details of this definition.' : 'Fill in the details for the new definition, this will apply to all Businesses.' }}
+                </span>
 
                 <form wire:submit.prevent="{{ $modelId ? 'update' : 'create' }}">
                     <div class="space-y-4">
--- resources/views/partials/provider-heading.blade.php
+++ resources/views/partials/provider-heading.blade.php
@@ -1,5 +1,5 @@
 <div class="relative mb-6 w-full">
-    <flux:heading size="xl" level="1">{{ __('Providers') }}</flux:heading>
-    <flux:subheading size="lg" class="mb-6">{{ __('Manage your providers') }}</flux:subheading>
+    <flux:heading size="xl" level="1">{{ __('Suppliers') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ __('Manage your suppliers') }}</flux:subheading>
     <flux:separator variant="subtle" />
 </div>
